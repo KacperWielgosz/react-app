82ee4650763e000bb78cbb26c15657c1c416e5b1
K W <k4cperwi3lgosz@gmail.com>
2022-03-09 19:16:23 +0100

    Add Container to App.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,17 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
   return (
-    <div className="list">
-        <header className="header">
-            <h2 className="title">Things to do<span>soon!</span></h2>
+    <div className={styles.list}>
+        <header className={styles.header}>
+            <h2 className={styles.title}>Things to do<span>soon!</span></h2>
         </header>
-        <p className="description">Interesting things I want to check out</p>
-        <section className="columns">
-          <article>
-            <h2 className="title">Books</h2>
-          </article>
-          <article>
-            <h2 className="title">Movies</h2>
-          </article>
-          <article>
-            <h2 className="title">Games</h2>
-          </article>
+        <p className={styles.description}>Interesting things I want to check out</p>
+        <section className={styles.columns}>
+          <Column title="Books" icon="book" />
+          <Column title="Movies" icon="film"/>
+          <Column title="Games" icon="gamepad"/>
         </section>
     </div>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,14 @@
 import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button'
 
 const SearchForm = () => {
     return (
-        <form className="searchForm">
-            <input type="text" className="input" />
-            <button className="button">Search</button>
+        <form className={styles.searchForm}>
+          <TextInput placeholder="Search..." />
+          <Button>
+            <span className="fa fa-search"/>
+          </Button>
         </form>
     );
   };
