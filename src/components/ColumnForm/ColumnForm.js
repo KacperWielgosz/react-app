7bd253a16f2e8703d21df803be646377a07a547b
K W <k4cperwi3lgosz@gmail.com>
2022-03-27 21:51:01 +0200

    Add useDispatch to CardForm, update reducer

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,24 +1,25 @@
 import styles from './ColumnForm.module.scss';
 import shortid from 'shortid';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 
-
-
 const ColumnForm = props => {
-  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: value, icon: icon});
-    setValue('');
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
+    setTitle('');
     setIcon('');
   }
 
   return (
     <form className={styles.columnForm} onSubmit={handleSubmit}>
-      <span> Title: </span> <input className={styles.columnForm} type="text" value={value} onChange={e => setValue(e.target.value)} />
+      <span> Title: </span> <input className={styles.columnForm} type="text" title={title} onChange={e => setTitle(e.target.value)} />
       <span> Icon: </span> <input className={styles.columnForm} type="text" value={icon} onChange={e => setIcon(e.target.value)} />
       <Button>Add column</Button>
     </form>
